f1c1c99f9354f62bf0bcc0415bb3f14c9e0fc27d
GYFeng <yuanfeng.guo@foxmail.com>
2017-01-16 11:10:42 +0800

    增加相应注释

--- activemq/src/main/java/team/study/mq/activemq/simple/HelloWordPublisher.java
+++ activemq/src/main/java/team/study/mq/activemq/simple/HelloWordPublisher.java
@@ -29,10 +29,14 @@ public class HelloWordPublisher {
             connection = connectionFactory.createConnection();
             // 开始连接
             connection.start();
+            // 创建会话，该会话中的消息都使用自动确认机制
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            // 创建发送的目的MQ
             Queue helloWorldQueue = session.createQueue("hello-world");
+            // 消息发送者
             producer = session.createProducer(helloWorldQueue);
 
+            // 循环发送消息
             for (int i = 0; i < 100; i++) {
                 ActiveMQTextMessage message = new ActiveMQTextMessage();
                 message.setText("hello-world:" + i);
@@ -41,27 +45,39 @@ public class HelloWordPublisher {
                 Thread.sleep(1000);
             }
         } finally {
-            try {
-                if (producer != null) {
-                    producer.close();
-                }
-            } catch (JMSException e) {
-                LOGGER.warn("close producer error", e);
+            // 关闭连接
+            closeConnection(connection, session, producer);
+        }
+    }
+
+    /**
+     * 关闭MQ连接，若关闭连接失败，不会抛出异常。会在日志中打印warn级别的日志
+     *
+     * @param connection MQ连接对象
+     * @param session    MQ Session对象
+     * @param producer   MQ发布对象
+     */
+    private static void closeConnection(Connection connection, Session session, MessageProducer producer) {
+        try {
+            if (producer != null) {
+                producer.close();
             }
-            try {
-                if (session != null) {
-                    session.close();
-                }
-            } catch (JMSException e) {
-                LOGGER.warn("close session error", e);
+        } catch (JMSException e) {
+            LOGGER.warn("close producer error", e);
+        }
+        try {
+            if (session != null) {
+                session.close();
             }
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (JMSException e) {
-                LOGGER.warn("close connection error", e);
+        } catch (JMSException e) {
+            LOGGER.warn("close session error", e);
+        }
+        try {
+            if (connection != null) {
+                connection.close();
             }
+        } catch (JMSException e) {
+            LOGGER.warn("close connection error", e);
         }
     }
 }
